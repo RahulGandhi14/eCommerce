be6686f926ed6ac941447c22747bf137c10d0419
Rahul Gandhi <>
2021-04-26 21:41:00 +0530

    removed unwanted part

--- index.js
+++ index.js
@@ -61,12 +61,4 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3001;
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
-
 app.listen(PORT,()=>console.log(`app is running on port - ${PORT}`));
